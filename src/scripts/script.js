afccb63f080c58e543777c750709193d5c3a1733
edineladelso <edineladelso@users.noreply.github.com>
2024-07-25 00:08:06 +0000

    Fim do projeto.
    Construi formulario tesla e está funcionando corretamente

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,8 @@
 // jotform para criar formulario
+const formulario = document.querySelector('#formulario')
+function chamarformulario(){
+    formulario.style.left = '50%'
+}
+function fecharFormulario(){
+    formulario.style.left = '-50%'
+}
